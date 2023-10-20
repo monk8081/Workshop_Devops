e2f5fdd52e02e6626aa5ca12fe5b3ab0cdb26325
Manish Maurya <mmaurya694@gmail.com>
2023-10-20 22:52:12 +0530

    modify terraform file

---
+++ Terraform/V1-EC2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "linux-KP"
+}
